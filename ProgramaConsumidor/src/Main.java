fa0a8430dead42885d3618f5303ea88acc157cc8
Gabriel Torres <gabrielstorres08@gmail.com>
2025-05-06 22:25:53 -0300

    Update Main.java
    Lógica criada para definir qual estrutura de dados será utilizada conforme o tipo de ordenação (Bubble/Quick Sort)

--- ProgramaConsumidor/src/Main.java
+++ ProgramaConsumidor/src/Main.java
@@ -1,12 +1,38 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        String caminhoEntrada = //"XXXX"//;
+        String caminhoEntrada = "C:\\Users\\Gabriel Torres\\Meu Drive (gabrielstorres08@gmail.com)\\Gabriel Torres\\IFF\\BSI\\BSI 4 Periodo\\Técnicas de Programação\\ProgramaConsumidor\\src\\marketing_campaign.csv";
         String caminhoSaida = "consumidores_filtrados.csv";
 
-        ColecaoConsumidor colecao = new LLDeConsumidores();
-        ManipulaDataset.lerDoArquivoCSV(colecao, caminhoEntrada);
-        System.out.println("Total de consumidores carregados: " + colecao.obterTotalDeConsumidores());
-        ManipulaDataset.escreverNoArquivoCSV(colecao, caminhoSaida);
+        File arquivoEntrada = new File(caminhoEntrada);
+        if (!arquivoEntrada.exists()) {
+            System.out.println("Arquivo de entrada não encontrado: " + caminhoEntrada);
+            return;
+        }
+
+        // Carregar os dados inicialmente em ambas as estruturas
+        ColecaoConsumidor lista = new LLDeConsumidores();
+        ColecaoConsumidor vetor = new VetorDeConsumidores();
+        ManipulaDataset.lerDoArquivoCSV(lista, caminhoEntrada);
+        ManipulaDataset.lerDoArquivoCSV(vetor, caminhoEntrada);
+        System.out.println("Total de consumidores carregados: " + lista.obterTotalDeConsumidores());
+
+        // Definir as ordenações
+        Ordenador ordenadorEscolaridade = new OrdenarPorEscolaridadeComBubbleSort();
+        Ordenador ordenadorAno = new OrdenarPorAnoDeNascimentoComQuickSort();
+
+        // Escolher a estrutura de dados com base no tipo de ordenação
+        ColecaoConsumidor colecaoEscolaridade = (ordenadorEscolaridade instanceof OrdenarPorEscolaridadeComBubbleSort) ? lista : vetor;
+        colecaoEscolaridade.ordenarConsumidores(ordenadorEscolaridade);
+        System.out.println("Total após ordenação por escolaridade (usando " + colecaoEscolaridade.getClass().getSimpleName() + "): " + colecaoEscolaridade.obterTotalDeConsumidores());
+
+        ColecaoConsumidor colecaoAno = (ordenadorAno instanceof OrdenarPorAnoDeNascimentoComQuickSort) ? vetor : lista;
+        colecaoAno.ordenarConsumidores(ordenadorAno);
+        System.out.println("Total após ordenação por ano de nascimento (usando " + colecaoAno.getClass().getSimpleName() + "): " + colecaoAno.obterTotalDeConsumidores());
+
+        // Escrever o resultado da última ordenação
+        ManipulaDataset.escreverNoArquivoCSV(colecaoAno, caminhoSaida);
         System.out.println("Arquivo salvo em: " + caminhoSaida);
     }
 }
